c2157e40d812a4c1230ef927530c930d24f5cb60
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2024-10-01 21:53:12 +0530

    new: made svg into react component

---
+++ entrypoints/popup/Down.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Down = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+             className="lucide lucide-arrow-down text-gray-600">
+            <path d="M12 5v14"/>
+            <path d="m19 12-7 7-7-7"/>
+        </svg>
+    )
+}
+export default Down
