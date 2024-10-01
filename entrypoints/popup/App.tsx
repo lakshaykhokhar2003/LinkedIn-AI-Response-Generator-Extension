d08ec7e0ac2a12c1a67bbf6da95f9c7de64bb269
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2024-10-01 21:49:32 +0530

    new: made it better

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,114 +1,144 @@
 import React, {useState} from "react";
+import Wand from "@/entrypoints/popup/Wand.tsx";
+import Down from "@/entrypoints/popup/Down.tsx";
+import Refresh from "@/entrypoints/popup/Refresh.tsx";
+import Send from "@/entrypoints/popup/Send.tsx";
 
 const App = () => {
     const [prompt, setPrompt] = useState('');
     const [response, setResponse] = useState('');
     const [showModal, setShowModal] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [showResponse, setShowResponse] = useState(false);
+    const [isRegenerate, setIsRegenerate] = useState(false); // New state for button
 
+    let Placeholder = 'Your Prompt';
     const generateResponse = async () => {
-        // Call OpenAI API
-        const result = await fetch('https://api.groq.com/openai/v1/chat/completions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${import.meta.env.VITE_API_KEY}`,
-            },
-            body: JSON.stringify({
-                model: 'llama-3.1-8b-instant',
-                messages: [
-                    {
-                        role: "system",
-                        content: "You are a helpful assistant."
-                    },
-                    {
-                        role: "user",
-                        content: prompt
-                    }
-                ],
-                max_tokens: 100,
-            }),
-        });
-        const data = await result.json();
-        console.log(data)
-        setResponse(data.choices[0].message.content);
+        if (prompt.trim() === '') Placeholder = 'Please enter a prompt';
+
+        setLoading(true);
+        setShowResponse(true);
+        setIsRegenerate(true);
+        try {
+            const result = await fetch('https://api.groq.com/openai/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${import.meta.env.VITE_API_KEY}`,
+                },
+                body: JSON.stringify({
+                    model: 'llama-3.1-8b-instant',
+                    messages: [
+                        {
+                            role: "system",
+                            content: "You are a helpful assistant, also known as a chatbot, assist and give advice to users as they want."
+                        },
+                        {
+                            role: "user",
+                            content: prompt
+                        }
+                    ],
+                    max_tokens: 100,
+                }),
+            });
+            const data = await result.json();
+            setResponse(data.choices[0].message.content);
+        } catch (error) {
+            console.error(error);
+            setResponse('Error generating response');
+        } finally {
+            setLoading(false);
+            Placeholder = 'Your Prompt';
+        }
     };
 
-    // const generateResponse = async () => {
-    //     const res = await axios.post('https://api.groq.com/openai/v1/chat/completions', {
-    //         model: 'llama-3.1-8b-instant',
-    //         messages: [{
-    //             role: "system",
-    //             content: "You are a helpful assistant."
-    //         }, {
-    //             role: "user",
-    //             content: prompt
-    //         }],
-    //         max_tokens: 100,
-    //     }, {
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Authorization': `Bearer ${import.meta.env.VITE_API_KEY}`,
-    //         }
-    //     })
-    //     setResponse(res.data.choices[0].message.content);
-    // }
-
     const insertResponse = async () => {
-        await chrome.runtime.sendMessage({action: 'insertMessage', content: response})
-        // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        //     console.log('Tabs:', tabs);  // Log the tabs to ensure you're getting them
-        //     if (tabs.length === 0 || !tabs[0].id) {
-        //         console.error('No active tab found!');
-        //         return;
-        //     }
-        //     chrome.tabs.sendMessage(tabs[0].id, { action: 'insertMessage', content: response }, (response) => {
-        //         if (chrome.runtime.lastError) {
-        //             console.error('Error sending message:', chrome.runtime.lastError.message);
-        //         } else {
-        //             console.log('Message sent successfully:', response);
-        //         }
-        //     });
-        // });
+        await chrome.runtime.sendMessage({action: 'insertMessage', content: response});
+        closeModal();
+    };
+
+    const closeModal = () => {
+        setPrompt('');
+        setResponse('');
+        setShowResponse(false);
+        setIsRegenerate(false);
+        setShowModal(false);
     };
 
 
     return (
-        <div className="p-4 h-[100vh] w-full">
+        <div className="p-4">
             <div
                 onClick={() => setShowModal(true)}
-                className="fixed bottom-4 right-4 bg-gray-300 p-2 rounded-full cursor-pointer">
-                Open
-                {/*<img src="/wxt.svg" alt="AI Icon" className="w-12 h-12"/>*/}
+                className="absolute bottom-4 right-2 bg-white p-2 rounded-full cursor-pointer z-10"
+            >
+                <Wand/>
             </div>
             {showModal && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-white p-4 rounded">
-                        <input
-                            type="text"
-                            value={prompt}
-                            onChange={(e) => setPrompt(e.target.value)}
-                            className="border p-2 w-full"
-                            placeholder="Enter command"
-                        />
-                        <button onClick={generateResponse} className="bg-green-500 text-white px-4 py-2 mt-2 rounded">
-                            Generate
-                        </button>
+                <div
+                    className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center outline-none"
+                    onClick={closeModal}
+                >
+                    <div
+                        className="relative bg-white p-4 rounded-lg mx-auto w-full max-w-[320px] overflow-hidden"
+                        onClick={(e) => e.stopPropagation()}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Escape') closeModal();
+                        }}
+                    >
+                        {showResponse && (
+                            <div className="flex flex-col space-y-4 mt-4 tracking-normal">
+                                {/* User's Typed Message */}
+                                <div className="max-w-[75%] w-auto p-4 rounded-lg bg-gray-200 self-end">
+                                    {prompt && <p className="text-gray-500">{prompt}</p>}
+                                </div>
 
-
-                        {response && (
-                            <div className="mt-4">
-                                <button onClick={() => setShowModal(false)} className="float-right">X</button>
-                                <p>{response}</p>
-                                <button onClick={insertResponse}
-                                        className="bg-blue-500 text-white px-4 py-2 rounded mt-2">
-                                    Insert into LinkedIn
-                                </button>
+                                {/* AI Response */}
+                                <div className="max-w-[75%] w-auto p-4 rounded-lg bg-blue-200 self-start">
+                                    {loading ? (
+                                        <div className="animate-pulse text-blue-500">Loading AI Response...</div>
+                                    ) : (
+                                        response && <p className="text-gray-500">{response}</p>
+                                    )}
+                                </div>
                             </div>
+
                         )}
+
+                        <input
+                            disabled={isRegenerate}
+                            type="text"
+                            onChange={(e) => setPrompt(e.target.value)}
+                            className="w-full p-2 mt-4 outline-none border border-gray-300 rounded"
+                            placeholder={Placeholder}
+                            onKeyDown={async (e) => {
+                                if (e.key === 'Enter') await generateResponse();
+                            }}
+                        />
+                        <div className="flex justify-end gap-5 mt-4">
+                            {(response && showResponse) && (
+                                <button
+                                    onClick={insertResponse}
+                                    className="flex flex-row justify-center items-center text-gray-500 border-solid border border-gray-600 font-semibold px-2 py-1 rounded-lg"
+                                >
+                                    <Down/>
+                                    Insert
+                                </button>
+                            )}
+                            <button
+                                onClick={generateResponse}
+                                className={`flex flex-row gap-2 justify-center items-center bg-blue-500 font-semibold text-white px-4 py-1 rounded-lg ${isRegenerate ? 'opacity-50 cursor-not-allowed' : ''}`}
+                                disabled={isRegenerate}
+                            >
+                                {isRegenerate ? <Refresh/> : <Send/>}
+                                {isRegenerate ? 'Regenerate' : 'Generate'}
+                            </button>
+                        </div>
                     </div>
                 </div>
             )}
         </div>
     );
 };
+
 export default App;
--- wxt.config.ts
+++ wxt.config.ts
@@ -1,26 +1,27 @@
-import { defineConfig } from 'wxt';
+import {defineConfig} from 'wxt';
+
 export default defineConfig({
- manifest:{
-     manifest_version: 3,
-     name: "LinkedIn AI Response Generator",
-     description: "Generates AI responses using OpenAI for LinkedIn messages.",
-     version: "1.0",
-     permissions: ["activeTab","tabs","storage", "scripting"],
-     background: {
-         service_worker: "src/background.js",
-     },
-     action: {
-         default_popup: "src/pages/popup.html",
-     },
-     content_scripts: [
-         {
-             matches: ["*://www.linkedin.com/*"],
-             js: ["content-scripts/content.js"],
-             css:['assets/popup-D2Tvdy5B.css']
-         },
-     ],
-     content_security_policy:{
+    manifest: {
+        manifest_version: 3,
+        name: "LinkedIn AI Response Generator",
+        description: "Generates AI responses using OpenAI for LinkedIn messages.",
+        version: "1.0",
+        permissions: ["activeTab", "tabs", "storage", "scripting"],
+        background: {
+            service_worker: "src/background.js",
+        },
+        action: {
+            default_popup: "src/pages/popup.html",
+        },
+        content_scripts: [
+            {
+                matches: ["*://www.linkedin.com/*"],
+                js: ["content-scripts/content.js"],
+                css: ['assets/popup-wwYWVWPp.css'],
+            },
+        ],
+        content_security_policy: {
             extension_pages: "script-src 'self'; object-src 'self';"
-     }
- }
+        }
+    }
 });
